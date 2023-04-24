017ab8d1ff7813bfa1bc20d1c12ca597475f389c
Marc Harvey-Hill <mnh33@cl.cam.ac.uk>
2023-04-24 09:55:53 +0100

    fixed node test 2

--- src/runExperiments.py
+++ src/runExperiments.py
@@ -34,7 +34,7 @@ with open(test_path + 'stats.csv', 'w') as f:
 	f.write('name, version, nodes, throughput, goodput, mean, sd, rec, sent, batch_size, msg_size\n')
 
 nodeCounts = [1, 2, 4, 7, 10, 13]
-nodeCounts = [4]
+#nodeCounts = [4]
 rates = [1, 10, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000, 4000]
 # rates = [100, 200, 500, 1000, 2000, 4000]
 # batch_sizes = [1, 50, 100, 300, 450, 600, 99999999]
