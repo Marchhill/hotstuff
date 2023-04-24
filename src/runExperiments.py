e2604679ed9ab692a40ca2f5352ce109c60fb274
Marc Harvey-Hill <mnh33@cl.cam.ac.uk>
2023-04-24 17:44:44 +0100

    ablation 1, chained

--- src/runExperiments.py
+++ src/runExperiments.py
@@ -10,18 +10,20 @@ import random
 
 processes = []
 
+
 def kill_processes():
-	for p in processes:
-		try:
-			os.killpg(os.getpgid(p.pid), signal.SIGKILL)
-		except:
-			print("already killed!")
+    for p in processes:
+        try:
+            os.killpg(os.getpgid(p.pid), signal.SIGKILL)
+        except:
+            print("already killed!")
+
 
 atexit.register(kill_processes)
 
 test_name = "test_" + datetime.now().strftime("%d-%m-%Y-%H:%M:%S")
 if len(sys.argv) >= 2:
-	test_name = sys.argv[1]
+    test_name = sys.argv[1]
 
 test_path = './experiments/data/' + test_name + '/'
 
@@ -31,37 +33,41 @@ os.mkdir(test_path + 'subtests')
 
 # create stats file
 with open(test_path + 'stats.csv', 'w') as f:
-	f.write('name, version, nodes, throughput, goodput, mean, sd, rec, sent, batch_size, msg_size\n')
+    f.write('name, version, nodes, throughput, goodput, mean, sd, rec, sent, batch_size, msg_size\n')
 
-nodeCounts = [1, 2, 4, 7, 10, 13]
-#nodeCounts = [4]
+# nodeCounts = [1, 2, 4, 7, 10, 13]
+nodeCounts = [4]
 rates = [1, 10, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000, 4000]
 # rates = [100, 200, 500, 1000, 2000, 4000]
 # batch_sizes = [1, 50, 100, 300, 450, 600, 99999999]
-batch_sizes = [100]
+batch_sizes = [9999999999]
 experiment_time = 10
 repeats = 3
-version = "x"
+version = "1"
 
 # randomise testing order
-test_iter = random.sample(list(itertools.product(rates, nodeCounts, batch_sizes)) * repeats, len(rates) * len(nodeCounts) * len(batch_sizes) * repeats)
+test_iter = random.sample(list(itertools.product(rates, nodeCounts, batch_sizes))
+                          * repeats, len(rates) * len(nodeCounts) * len(batch_sizes) * repeats)
 
 x = 0
 # randomise order of tests (generate permutations...)
 for (rate, n, s) in test_iter:
-	succ = False
-	while not succ:
-		name = f'{test_name}_{x}_{rate}_{n}_{s}'
-		print(f'running "{name}"')
-		for i in range(n):
-			processes.append(subprocess.Popen(f'ulimit -n 65536; eval $(opam env) dune exec --build-dir=_build{str(i)} -- ./main.exe -i {str(i)} -n {str(n)} -b {str(s)}', shell=True, preexec_fn=os.setsid))
-		time.sleep(5)
-		completed = subprocess.run(f'eval $(opam env) dune exec -- ./live_test.exe {str(n)} -v {version} -t {str(experiment_time)} -r {str(rate)} -b {str(s)} --times "{test_path + "subtests/" + name}.csv" --stats "{test_path}stats.csv"', shell=True)
-		if completed.returncode == 0:
-			succ = True
-		time.sleep(1)
-		kill_processes()
-		processes = []
-	x += 1
+    succ = False
+    while not succ:
+        name = f'{test_name}_{x}_{rate}_{n}_{s}'
+        print(f'running "{name}"')
+        for i in range(n):
+            processes.append(subprocess.Popen(
+                f'ulimit -n 65536; eval $(opam env) dune exec --build-dir=_build{str(i)} -- ./main.exe -i {str(i)} -n {str(n)} -b {str(s)}', shell=True, preexec_fn=os.setsid))
+        time.sleep(5)
+        cmd = f'eval $(opam env) dune exec -- ./live_test.exe {str(n)} -t {str(experiment_time)} --version "{version}" -r {str(rate)} -b {str(s)} --times "{test_path + "subtests/" + name}.csv" --stats "{test_path}stats.csv"'
+        print(cmd)
+        completed = subprocess.run(cmd, shell=True)
+        if completed.returncode == 0:
+            succ = True
+        time.sleep(1)
+        kill_processes()
+        processes = []
+    x += 1
 
-subprocess.run(f'python3 ./experiments/plot.py {test_name}', shell = True)
+subprocess.run(f'python3 ./experiments/plot.py {test_name}', shell=True)
