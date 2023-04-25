ea43c95e51ab8f6ecc41e4733d788312f5373892
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2023-04-25 23:51:33 +0100

    wrote some evaluation, new ablation study

--- src/experiments/plot.py
+++ src/experiments/plot.py
@@ -19,45 +19,45 @@ if not os.path.exists(graph_dir):
 if not os.path.exists(subtests_dir):
 	os.mkdir(subtests_dir)
 
-# file_stats = []
-# for file in os.listdir(f'./experiments/data/{test_name}/subtests'):
-# 	filename = os.fsdecode(file)
-# 	times = pd.read_csv(f'./experiments/data/{test_name}/subtests/{filename}', skipinitialspace=True)
-# 	filename = os.path.splitext(filename)[0]
-# 	times['sent(s)'] = times['sent'] / 1_000_000_000
-# 	times['latency'] = (times['rec'] - times['sent']) / 1_000_000_000
-# 	file_stats.append({
-# 		'name': filename,
-# 		'medianlatency': times['latency'].median(),
-# 		'99latency': times['latency'].quantile(0.99),
-# 		'meanlatency': times['latency'].mean()
-# 	})
+file_stats = []
+for file in os.listdir(f'./experiments/data/{test_name}/subtests'):
+	filename = os.fsdecode(file)
+	times = pd.read_csv(f'./experiments/data/{test_name}/subtests/{filename}', skipinitialspace=True)
+	filename = os.path.splitext(filename)[0]
+	times['sent(s)'] = times['sent'] / 1_000_000_000
+	times['latency'] = (times['rec'] - times['sent']) / 1_000_000_000
+	file_stats.append({
+		'name': filename,
+		'medianlatency': times['latency'].median(),
+		'99latency': times['latency'].quantile(0.99),
+		'meanlatency': times['latency'].mean()
+	})
 
-# 	# cumulative latency
-# 	ax = sns.ecdfplot(x='latency', data=times, palette = color)
-# 	ax.set(xlabel = 'latency (s)', ylabel = 'fraction of requests', xscale = 'log')
-# 	fig = ax.get_figure()
-# 	fig.savefig(f'{subtests_dir + filename}_cumlatency.png')
-# 	plt.close(fig)
+	# cumulative latency
+	ax = sns.ecdfplot(x='latency', data=times, palette = color)
+	ax.set(xlabel = 'latency (s)', ylabel = 'fraction of requests', xscale = 'log')
+	fig = ax.get_figure()
+	fig.savefig(f'{subtests_dir + filename}_cumlatency.png')
+	plt.close(fig)
 
-# 	# time / latency heatmap
-# 	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
-# 	# ax = sns.histplot(times, x="sent(s)", y="latency", binwidth=(0.05, 10), ax=ax)
-# 	ax = sns.histplot(times, x="sent(s)", y="latency", bins = 50, ax=ax, palette = color)
-# 	ax.set(xlabel="time (s)", xlim=(0,10), ylabel = "latency (s)")
-# 	fig.savefig(f'{subtests_dir + filename}_timelatencyheatmap.png')
-# 	plt.close(fig)
+	# time / latency heatmap
+	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
+	# ax = sns.histplot(times, x="sent(s)", y="latency", binwidth=(0.05, 10), ax=ax)
+	ax = sns.histplot(times, x="sent(s)", y="latency", bins = 50, ax=ax, palette = color)
+	ax.set(xlabel="time (s)", xlim=(0,10), ylabel = "latency (s)")
+	fig.savefig(f'{subtests_dir + filename}_timelatencyheatmap.png')
+	plt.close(fig)
 
-# 	# time / latency heatmap (log)
-# 	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
-# 	# ax = sns.histplot(times, x="sent(s)", y="latency", binwidth=(0.05, 10), ax=ax)
-# 	ax = sns.histplot(times, x="sent(s)", y="latency", bins = 50, ax=ax, palette = color)
-# 	ax.set(xlabel="time (s)", xlim=(0,10), ylabel = "latency (s)", yscale = 'log')
-# 	fig.savefig(f'{subtests_dir + filename}_timelatencyheatmap_log.png')
-# 	plt.close(fig)
+	# time / latency heatmap (log)
+	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
+	# ax = sns.histplot(times, x="sent(s)", y="latency", binwidth=(0.05, 10), ax=ax)
+	ax = sns.histplot(times, x="sent(s)", y="latency", bins = 50, ax=ax, palette = color)
+	ax.set(xlabel="time (s)", xlim=(0,10), ylabel = "latency (s)", yscale = 'log')
+	fig.savefig(f'{subtests_dir + filename}_timelatencyheatmap_log.png')
+	plt.close(fig)
 
-# file_stats_df = pd.DataFrame(file_stats)
-# print(file_stats_df)
+file_stats_df = pd.DataFrame(file_stats)
+print(file_stats_df)
 
 # open stats file and add column
 stats = pd.read_csv(f'./experiments/data/{test_name}/stats.csv', skipinitialspace=True)
@@ -85,6 +85,7 @@ plt.close(fig)
 ax = sns.lineplot(x='goodput', y='latencyfiltered', data=stats, hue='batch_size', palette = color, linestyle='--')
 ax.set(xlabel = 'goodput (req/s)', ylabel = 'mean latency (s)')
 ax.legend(title = 'batch size')
+ax.invert_yaxis()
 fig = ax.get_figure()
 fig.savefig(graph_dir + 'goodputlatency.png')
 plt.close(fig)
@@ -116,6 +117,7 @@ plt.close(fig)
 
 ax = sns.lineplot(x='goodput', y='latencyfiltered', data=stats, hue='nodes', palette = color, linestyle='--')
 ax.set(xlabel = 'goodput (req/s)', ylabel = 'mean latency (s)')
+ax.invert_yaxis()
 fig = ax.get_figure()
 fig.savefig(graph_dir + 'goodputlatency_nodes.png')
 plt.close(fig)
@@ -144,6 +146,7 @@ plt.close(fig)
 ax = sns.lineplot(x='goodput', y='latencyfiltered', data=stats, hue='version', palette = color, linestyle='--')
 ax.set(xlabel = 'goodput (req/s)', ylabel = 'mean latency (s)')
 ax.legend(title = 'version')
+ax.invert_yaxis()
 fig = ax.get_figure()
 fig.savefig(graph_dir + 'goodputlatency_ablation.png')
 plt.close(fig)
